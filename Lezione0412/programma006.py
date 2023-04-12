4634d656623fb379c122529044609ff216867f75
Luca Pennini <luca198871@gmail.com>
2023-04-12 14:20:42 +0200

    updated 0412 folder with write-read examples

--- Lezione0412/programma006.py
+++ Lezione0412/programma006.py
@@ -6,11 +6,43 @@ def writelines(file, lista):
         file.write(linea+"\n")
 
 
+res = []
 with open("./Lezione0412/prova2.txt", "wt") as file:
     i = 0
-    while True:
+    while i == 4:
         i += 1
         file.write("ciao\nLuca")
         writelines(file, ["Ciao ancora", "Bella bionda"])
         time.sleep(3)
         print(i)
+
+with open("./Lezione0412/laboratorio.txt") as stream:
+    L = []
+    for item in stream:
+        # spazio
+        L = (item.strip()).split(":")
+        spazio = L[0]
+        tempi = L[1]
+        numeri = (tempi.strip()).split()
+        media = 0
+        for tempo in numeri:
+            media += float(spazio)/float(tempo)
+        media /= 4
+        res.append(media)
+
+
+def applica_a_tutti(lista, funzione):
+    n_lista = []
+    for x in lista:
+        n_lista += [funzione(x)]
+    return n_lista
+
+
+with open("./Lezione0412/prova2.txt", "wt") as file:
+    res = applica_a_tutti(res, str)
+    # res = res.apply(str) #valida sula libreria numpy
+    # res = [str(x) for x in res]
+    stringa = " - ".join(res)
+    print(stringa)
+
+print("-"*20)
