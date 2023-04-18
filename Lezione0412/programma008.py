b24e3a230f5b437abf4c65aef6075c1c6815b9af
Luca88ita <luca198871@gmail.com>
2023-04-18 23:11:20 +0200

    updated prog8

--- Lezione0412/programma008.py
+++ Lezione0412/programma008.py
@@ -4,7 +4,7 @@
 # definire a funzione add e multiply che accetta un'altra fraction e modifichi la frazione corrente aggiungendo ad essa/moltiplicandola
 # definire gli operatori +,*,==,<
 
-""" import math
+import math
 
 
 class Fraction:
@@ -22,24 +22,116 @@ class Fraction:
         self.MCD()
 
     def MCD(self):
-
-        mcd = math.gcd(self.numeratore, self.denominatore)
-        print(mcd)
-
-        self.numeratore = int(self.numeratore/mcd)
-        self.denominatore = int(self.denominatore/mcd)
+        numeratore = self.numeratore
+        denominatore = self.denominatore
+        mcd = math.gcd(numeratore, denominatore)
+        numeratore = int(numeratore / mcd)
+        denominatore = int(denominatore / mcd)
 
     def __str__(self):
         if self.denominatore == 1:
-            return (f"{self.numeratore}")
+            return f"{self.numeratore}"
         else:
-            return (f"{self.numeratore}/{self.denominatore}")
+            return f"{self.numeratore}/{self.denominatore}"
+
+    def __add__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        numeratore = (self.numeratore * mult1) + (other.numeratore * mult2)
+        denominatore = self.denominatore * mult1
+        mcd = math.gcd(int(numeratore), int(denominatore))
+        return f"{int(numeratore / mcd)}/{int(denominatore / mcd)}"
+
+    def __sub__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        numeratore = (self.numeratore * mult1) - (other.numeratore * mult2)
+        denominatore = self.denominatore * mult1
+        mcd = math.gcd(int(numeratore), int(denominatore))
+        return f"{int(numeratore / mcd)}/{int(denominatore / mcd)}"
 
     def __mul__(self, other):
         numeratore = self.numeratore * other.numeratore
         denominatore = self.denominatore * other.denominatore
+        mcd = math.gcd(numeratore, denominatore)
+        return f"{int(numeratore / mcd)}/{int(denominatore / mcd)}"
+
+    def __truediv__(self, other):
+        numeratore = self.numeratore * other.denominatore
+        denominatore = self.denominatore * other.numeratore
+        mcd = math.gcd(numeratore, denominatore)
+        return f"{int(numeratore / mcd)}/{int(denominatore / mcd)}"
+
+    def __eq__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) == (other.numeratore * mult2):
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) != (other.numeratore * mult2):
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) < (other.numeratore * mult2):
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) > (other.numeratore * mult2):
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) <= (other.numeratore * mult2):
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        mcm = math.lcm(self.denominatore, other.denominatore)
+        mult1 = mcm / self.denominatore
+        mult2 = mcm / other.denominatore
+        if (self.numeratore * mult1) >= (other.numeratore * mult2):
+            return True
+        else:
+            return False
 
 
-a = Fraction(-100, -10)
-print(a)
- """
+a = Fraction(5, -2)
+b = Fraction(-3, -2)
+
+c = d = Fraction(-1, 4)
+d = Fraction(1, 8)
+
+print(f"Moltiplicazione = {a * b}")  # -15/4
+print(f"Somma = {a + b}")  # -1/1
+print(f"Divisione = {a / b}")  # -5/3
+print(f"Sottrazione = {a - b}")  # -4/1
+print(f"Uguaglianza = {c == d}")  # False
+print(f"Disuguaglianza = {c != d}")  # True
+print(f"Maggiore => {c > d}")
+print(f"Minore => {c < d}")
+print(f"Maggiore o uguale => {c >= d}")
+print(f"Minore o uguale => {c <= d}")
