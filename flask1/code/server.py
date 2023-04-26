c49bb938657711705986f353a714fdfd176e1441
Luca Pennini <luca198871@gmail.com>
2023-04-26 15:49:30 +0200

    added sum function in calc

--- flask1/code/server.py
+++ flask1/code/server.py
@@ -1,4 +1,5 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 
@@ -29,9 +30,25 @@ def pagina2():
     return flask.render_template("pagina2.html")
 
 
-@app.route("/calcolatrice", methods=["POST", "GET"])
+@app.route("/calcolatrice")
 def calcolatrice():
     return flask.render_template("calcolatrice.html")
+
+
+@app.route("/calcolatriceAPI", methods=["POST", "GET"])
+def calcolatriceAPI():
+    GET = flask.request.args  # per leggere i parametri GET
+    POST = flask.request.get_data()  # per leggere i parametri POST
+    JSON = flask.request.get_json()  # per leggere i parametri POST JSON
+
+    print(JSON)
+
+    num1 = float(JSON["num1"])
+    num2 = float(JSON["num2"])
+
+    result = num1 + num2
+
+    return json.dumps(result)
 
 
 app.run("0.0.0.0", 8080, True)
