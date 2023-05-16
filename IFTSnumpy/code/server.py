41395f86490baa3d346fce95bcc28dc946b2ff60
Luca Pennini <luca198871@gmail.com>
2023-05-16 15:04:21 +0200

    completed google drive exercise

--- IFTSnumpy/code/server.py
+++ IFTSnumpy/code/server.py
@@ -10,51 +10,65 @@ def index():
     return flask.render_template("upload.html")
 
 
-@app.route("/uploader", methods=["POST"])
-def uploader():
-    # uploaded = flask.request.files["file"]
+@app.route("/uploader/<path:path>", methods=["POST"])
+def uploader(path):
     uploaded = flask.request.files.getlist("file")
-    # print(uploaded.save(f"uploaded/{uploaded.filename}"))
+    for f in uploaded:
+        f.save(f"uploads/{path}{f.filename}")
+        print(f"uploads/{path}{f.filename}")
+    return json.dumps(
+        {"success": True, "message": "Caricamento avvenuto con successo!"}
+    )
+
+
+@app.route("/uploadernopath", methods=["POST"])
+def uploadernopath():
+    uploaded = flask.request.files.getlist("file")
     for f in uploaded:
         f.save(f"uploads/{f.filename}")
-    return "Caricamento avvenuto con successo"
+        print(f"uploads/{f.filename}")
+    return json.dumps(
+        {"success": True, "message": "Caricamento avvenuto con successo!"}
+    )
 
 
-@app.route("/create_folder/<path:path>", methods=["POST"])
+@app.route("/create_folder/<path:path>", methods=["GET", "POST"])
 def create_folder(path):
-    print(path)
     if os.path.exists(f"uploads/{path}"):
-        return json.dumps({"success": False, "error": "folder already existing"})
+        return json.dumps({"success": False, "error": "la cartella esiste"})
     else:
         os.mkdir(f"uploads/{path}")
-        return json.dumps({"success": True, "folder": path})
+    return json.dumps({"success": True, "folder": path})
 
 
-@app.route("/folders/")
+@app.route("/folders")
 def folders():
     return flask.render_template("folders.html")
+
+
+"""
+il parametro json folder vuole / alla fine se è una sottocartella
+"""
 
 
 @app.route("/read_folder", methods=["POST"])
 def read_folder():
     folder = flask.request.get_json()["folder"]
-    if os.path.exists(f"uploads/{folder}"):
-        folder_content = os.listdir(f"uploads/{folder}")
-        folders_to_show = []
-        files_to_show = []
-        for x in folder_content:
-            i = f".../{folder}" + x
-            x = f"uploads/{folder}" + x
 
-            if os.path.isdir(x):
-                folders_to_show += [x]
-            if os.path.isfile(x):
-                files_to_show += [x]
-        return json.dumps(
-            {"success": True, "folders": folders_to_show, "files": files_to_show}
-        )
+    if os.path.exists(f"uploads/{folder}"):
+        tutto = os.listdir(f"uploads/{folder}")
+        dir = []
+        fls = []
+        for x in tutto:
+            y = f"uploads/{folder}" + x
+            x = f"{folder}" + x
+            if os.path.isdir(y):
+                dir += [x]
+            if os.path.isfile(y):
+                fls += [x]
+        return json.dumps({"success": True, "folders": dir, "files": fls})
     else:
-        return json.dumps({"success": True, "error": "folder does not exist"})
+        return json.dumps({"success": False})
 
 
 @app.route("/get_file/<path:path>")
@@ -62,52 +76,4 @@ def get_file(path):
     return flask.send_file(f"../uploads/{path}")
 
 
-"""
-@app.route("/static/<path:path>")
-def static_files(path):
-    return flask.send_from_directory("static", path)
-
-
-@app.route("/purchase", methods=["POST", "GET"])
-def purchase():
-    cart = flask.request.get_json()
-    for item in cart:
-        print(item)
-    return json.dumps({"success": True})
-
-
-@app.route("/login")
-def login():
-    nome = flask.request.args.get("nome", "")
-    if nome == "luca":
-        risposta = flask.make_response("benvenuto")
-        risposta.set_cookie("accesso", "true")
-    else:
-        risposta = flask.make_response("nome utente non trovato")
-    return risposta
-
-
-@app.route("/verification")
-def verification():
-    return "ciao"
-
-
-@app.route("/attacco")
-def attacco():
-    return flask.render_template("attacco.html")
-
-
-@app.route("/tesoro")
-def tesoro():
-    accesso = flask.request.cookies.get("accesso", "false")
-    if accesso == "true":
-        risposta = flask.make_response("OnePiece")
-    elif accesso == "false":
-        risposta = flask.make_response("bloccato")
-    else:
-        risposta = flask.make_response("sei un attaccante")
-    risposta.set_cookie("accesso", "false")
-    return risposta
-"""
-
-app.run("0.0.0.0", 8080, True)
+app.run("0.0.0.0", 5000, True)
